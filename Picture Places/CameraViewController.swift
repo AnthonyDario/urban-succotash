24320f843e41aa4fc76d23d615c3892a568a2a81
Anthony Dario <anthonyrdario@gmail.com>
2016-04-27 18:34:28 -0400

    I think i got camera to display

--- Picture Places/CameraViewController.swift
+++ Picture Places/CameraViewController.swift
@@ -8,6 +8,37 @@
 
 import UIKit
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if (UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)) {
+            //load the camera interface
+            
+            let picker : UIImagePickerController = UIImagePickerController()
+            picker.sourceType = UIImagePickerControllerSourceType.Camera
+            picker.delegate = self
+            picker.allowsEditing = false
+            self.presentViewController(picker, animated: true, completion: nil)
+            
+        }else{
+            //no camera available
+            
+            let alert = UIAlertController(title: "Error", message: "There is no camera available", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "Okay", style: .Default, handler: {(alertAction)in
+                alert.dismissViewControllerAnimated(true, completion: nil)
+            }))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+    
+    // MARK: UIImagePickerDelegate
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        imageView.image = image
+        self.dismissViewControllerAnimated(true, completion: nil);
+    }
+    
 }
